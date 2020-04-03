6c9b822641d04a59cc354c40e9b465977d640023
Bowenschlah <63106062+Bowenschlah@users.noreply.github.com>
2020-04-03 14:53:26 +0200

    Added Dashboard

---
+++ Downloads/forward-football-t1-master-Dashboard/dashboard/Dashboard/README.md
@@ -0,0 +1,8 @@
+# forwardfootballDash
+Dash implementation of dashboard
+
+You should be able to clone this repository and given that you have set up your database connection, you can run the app with 
+```
+python index.py 
+```
+from the commandline.
---
+++ Downloads/forward-football-t1-master-Dashboard/dashboard/Dashboard/apps/home.py
@@ -0,0 +1,16 @@
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from apps import player, club, coach, commonmodules
+
+from app import app
+
+
+layout = html.Div([
+    commonmodules.get_header(),
+    # dbc.Row(dbc.Col(html.Div('Forward Football Dashboard'))),
+    commonmodules.get_menu(),
+    html.Br(),
+    html.H3('This is home screen'),
+])
